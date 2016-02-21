c27b6acdc9d1bf9e3cdbf02aed6f262f659c702c
Omar Kohl <omarkohl@gmail.com>
2016-02-21 13:13:54 +0100

    Simplify home view and add search view

--- xss_demo/views.py
+++ xss_demo/views.py
@@ -1,6 +1,13 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/mytemplate.pt')
+@view_config(route_name='home', renderer='templates/home.pt')
 def my_view(request):
-    return {'project': 'xss-demo'}
+    return {}
+
+
+@view_config(route_name='search', renderer='templates/search.pt')
+def search(request):
+    return {'query': request.params.get('q', '')}
+    # http://localhost:6543/search?q=%3Cscript%3Ealert(123)%3C/script%3E
+    # chromium-browser --temp-profile --disable-xss-auditor
