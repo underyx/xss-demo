6ca02acff084734e747d992079014db23a24f500
Omar Kohl <omarkohl@gmail.com>
2016-02-28 21:58:20 +0100

    Add minimal content to DB on app startup

--- xss_demo/__init__.py
+++ xss_demo/__init__.py
@@ -1,4 +1,24 @@
 from pyramid.config import Configurator
+from .models import (
+    DB,
+    Post,
+    Comment,
+    )
+
+
+def _init_db():
+    p = Post('My first blogpost', 'Administrator')
+    DB.save(p)
+    c1 = Comment('Great post!', 'Paul E.', post_id=p.id)
+    c2 = Comment(
+        'Well written but I disagree with your conclusion.',
+        'Max M.',
+        post_id=p.id
+        )
+    DB.save(c1)
+    DB.save(c2)
+    p.comment_ids = [c1.id, c2.id]
+    DB.save(p)
 
 
 def main(global_config, **settings):
@@ -12,4 +32,5 @@ def main(global_config, **settings):
     config.add_route('search', '/search')
     config.add_route('search_raw', '/search_raw')
     config.scan()
+    _init_db()
     return config.make_wsgi_app()
